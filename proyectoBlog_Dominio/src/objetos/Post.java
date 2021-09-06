749d1e95994ff48ba4657617fe5cfc8bdc94aa03
MariaEncinasCar <90160554+MariaEncinasCar@users.noreply.github.com>
2021-09-05 17:31:41 -0700

    Add files via upload

---
+++ proyectoBlog_Dominio/src/objetos/Post.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package objetos;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Equipo 4
+ */
+public class Post {
+    private Date fechaHoraCreacion;
+    private String titulo;
+    private String contenido;
+    private Date fechaHoraEdicion;
+}
---
+++ proyectoBlog_Dominio/src/objetos/Usuario.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package objetos;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Equipo 4
+ */
+public class Usuario {
+    private String nombreCompleto;
+    private String correo;
+    private String contrasena;
+    private String telefono;
+    private String avatar;
+    private String ciudad;
+    private Date fechaNacimiento;
+    private char genero;
+}
